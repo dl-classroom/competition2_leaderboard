e71b46b8724e22274ed4a30dc376cbb7f724c966
Guri Zabergja <gurizabergja96@gmail.com>
2023-04-02 03:12:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 const leaderboardData = [
   // Example data
-  { rank: 1, team: 'Sample Model', submission: '1/5', score: 'TODO', date: '2023-03-21' },
+  { rank: 1, team: 'Sample Model', submission: 1, score: 95, date: '2023-03-21' },
+  { rank: 2, team: 'Sample Model', submission: 2, score: 90, date: '2023-03-22' },
   // Add more leaderboard data here
 ];
 
 const tableBody = document.getElementById('table-body');
 
-leaderboardData.forEach((entry) => {
-  const row = document.createElement('tr');
+// Group scores by team
+const groupedData = leaderboardData.reduce((acc, entry) => {
+  if (!acc[entry.team]) {
+    acc[entry.team] = [];
+  }
+  acc[entry.team].push(entry);
+  return acc;
+}, {});
 
-  Object.keys(entry).forEach((key) => {
-    const cell = document.createElement('td');
-    cell.textContent = entry[key];
-    row.appendChild(cell);
+// Sort scores within each team by descending score
+Object.values(groupedData).forEach((teamScores) =>
+  teamScores.sort((a, b) => b.score - a.score)
+);
+
+// Create table rows
+Object.entries(groupedData).forEach(([team, scores]) => {
+  scores.forEach((entry, index) => {
+    const row = document.createElement('tr');
+    row.classList.add(`team-${team.replace(/\s+/g, '-')}`);
+
+    if (index > 0) {
+      row.style.display = 'none';
+    }
+
+    Object.keys(entry).forEach((key) => {
+      const cell = document.createElement('td');
+      cell.textContent = entry[key];
+
+      if (key === 'team') {
+        cell.classList.add('team-cell');
+        cell.style.cursor = 'pointer';
+      }
+
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
   });
-
-  tableBody.appendChild(row);
 });
+
+// Toggle scores event listener
+tableBody.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('team-cell')) {
+    return;
+  }
+
+  const team = event.target.textContent;
+  const teamRows = document.querySelectorAll(`.team-${team.replace(/\s+/g, '-')}`);
+
+  teamRows.forEach((row, index) => {
+    if (index > 0) {
+      row.style.display = row.style.display === 'none' ? 'table-row' : 'none';
+    }
+  });
+});
+
