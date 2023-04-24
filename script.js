8d32db9a217ac0fdd5044c78dd228c9d900718e0
Guri Zabergja <gurizabergja96@gmail.com>
2023-04-24 16:07:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const leaderboardData = [
   // Example data
   { rank: 1, team: 'Sample Model', submission: 1, score: 23.358, date: '2023-03-21' },
   { rank: 2, team: 'NLL', submission: 1, score: 20.671, date: '2023-04-15' },
+  { rank: 3, team: 'imageseg2023', submission: 1, score: 14.964, date: '2023-04-22'},
   // Add more leaderboard data here
 ];
 
